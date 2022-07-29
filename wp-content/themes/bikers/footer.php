36b53461fc9119793a4393b060d627cf58538878
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-29 19:56:25 +0530

    fix filter search function for load more ajax call

--- wp-content/themes/bikers/footer.php
+++ wp-content/themes/bikers/footer.php
@@ -1,11 +1,5 @@
     </main>
     <!--main section end-->
-    <!--footer section start-->
-    <footer>
-      <div class="wrapper">
-      </div>
-    </footer>
-    <!--footer section end-->
   </div>
   <!--container end-->
   <?php wp_footer(); ?>
